991404a4e02d7b8bc17a09e444d5dfd998321337
MoeyJac <jo3ymac@gmail.com>
2023-06-06 22:34:34 -0400

    feat: basic deployment to ganache network

--- deploy.js
+++ deploy.js
@@ -1,7 +1,35 @@
-async function hi() {
-  console.log("Hi");
-  let variable = 5;
-  console.log("5");
+const ethers = require("ethers");
+const fs = require("fs");
+
+async function main() {
+  // RPC Server http://127.0.0.1:7545
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+
+  const wallet = new ethers.Wallet(
+    "0x4a3c12de5ed1ffd1d8c81e1ea2aa26b463adc052778fb652d345c2f4638232d0",
+    provider
+  );
+
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf-8"
+  );
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+
+  console.log("Deploying please wait...");
+
+  const contract = await contractFactory.deploy();
+  const transactionReceipt = await contract.deployTransaction.wait(1);
+
+  // console.log("Deployment Transaction: ");
+  // console.log(contract.deployTransaction);
+
+  // console.log("Transaction Receipt:");
+  // console.log(transactionReceipt);
 }
 
 main()
